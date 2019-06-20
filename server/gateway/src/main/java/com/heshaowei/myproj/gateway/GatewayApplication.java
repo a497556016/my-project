9e1df933153139e6fd82e5644d68893d1391026b
heshaowei <heshaowei@huacenter.com>
2019-06-20 09:44:00 +0800

    我们抛弃了eureka，决定临幸consul

--- server/gateway/src/main/java/com/heshaowei/myproj/gateway/GatewayApplication.java
+++ server/gateway/src/main/java/com/heshaowei/myproj/gateway/GatewayApplication.java
@@ -1,9 +1,7 @@
 package com.heshaowei.myproj.gateway;
 
-import com.heshaowei.myproj.gateway.filter.FilterProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
